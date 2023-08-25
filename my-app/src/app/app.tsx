ebaca348798914be1073552a1e8f144c7d419ebb
Patrick Sara <patricktheodoresara@gmail.com>
2023-08-25 19:41:56 +0800

    init

---
+++ my-app/src/app/app.tsx
@@ -0,0 +1,19 @@
+const App = (): JSX.Element => {
+  return (
+    <main className="bg-lime-400">
+      <header>
+        I am a header
+      </header>
+
+      <section>
+        I am a section
+      </section>
+      
+      <footer className="">
+        And I, a footer
+      </footer>
+    </main>
+  );
+};
+
+export default App;
